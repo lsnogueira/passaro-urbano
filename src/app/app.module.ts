6519bea0eb76cb7486244e06380414bcf9a6f68e
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-02-21 10:29:26 -0300

    add produtos dinamicos com api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -16,7 +16,8 @@ import { RodapeComponent } from './rodape/rodape.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,67 +1,16 @@
 import { Oferta } from './shared/oferta.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import 'rxjs/add/operator/toPromise';
 
+@Injectable()
 export class OfertasService {
+    
+    constructor(private http: HttpClient){}
 
-    public ofertas: Oferta[] = [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/1/img1.jpg" },
-                { url: "/assets/ofertas/1/img2.jpg" },
-                { url: "/assets/ofertas/1/img3.jpg" },
-                { url: "/assets/ofertas/1/img4.jpg" }
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/2/img1.jpg" },
-                { url: "/assets/ofertas/2/img2.jpg" },
-                { url: "/assets/ofertas/2/img3.jpg" },
-                { url: "/assets/ofertas/2/img4.jpg" }
-            ]
-
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/3/img1.jpg" },
-                { url: "/assets/ofertas/3/img2.jpg" },
-                { url: "/assets/ofertas/3/img3.jpg" },
-                { url: "/assets/ofertas/3/img4.jpg" },
-                { url: "/assets/ofertas/3/img5.jpg" },
-                { url: "/assets/ofertas/3/img6.jpg" }
-            ]
-        }
-    ]
-
-    public getOfertas(): Promise<Oferta[]> {
-        return new Promise((resolve, reject) => {
-            let erro: boolean;
-            erro = true;
-            if (!erro) {
-                resolve(this.ofertas);
-            } else {
-                reject({codError: 404, msgError: 'Not Found'});
-            }
-        })
+    public getOfertas(): Promise<Oferta[]> {                
+        return this.http.get('http://localhost:3000/ofertas')
+        .toPromise()
+        .then((resposta: any) => resposta);        
     }
 }
