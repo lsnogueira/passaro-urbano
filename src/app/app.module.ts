88e44bfe8cb445f63b30e8b99db4d92412c44241
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-02-27 00:21:41 -0300

    melhorias e novos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,    
+    DiversaoComponent,
+    RestaurantesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,12 +5,12 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class OfertasService {
-    
+
     constructor(private http: HttpClient){}
 
-    public getOfertas(): Promise<Oferta[]> {                
+    public getOfertas(): Promise<Oferta[]> {
         return this.http.get('http://localhost:3000/ofertas')
         .toPromise()
-        .then((resposta: any) => resposta);        
+        .then((resposta: any) => resposta);
     }
 }
