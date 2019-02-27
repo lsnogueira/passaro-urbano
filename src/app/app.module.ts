72ea99452342cc29bcb5dab9c7b0c210cc2bee55
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-02-27 00:42:51 -0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,24 @@ import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent,    
+    RodapeComponent,
     DiversaoComponent,
     RestaurantesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
