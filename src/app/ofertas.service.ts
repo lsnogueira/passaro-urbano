8fb176e6c87391cf9c00bfab51fac5de39589aad
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-03-25 15:57:52 -0300

    ident

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,18 +5,19 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class OfertasService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient){}
+  public getOfertas(): Promise<Oferta[]> {
+    return this.http
+      .get('http://localhost:3000/ofertas')
+      .toPromise()
+      .then((resposta: any) => resposta);
+  }
 
-    public getOfertas(): Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas')
-        .toPromise()
-        .then((resposta: any) => resposta);
-    }
-
-    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
-        .toPromise()
-        .then((oferta: Oferta[]) => oferta);
-    }
+  public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+    return this.http
+      .get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .toPromise()
+      .then((oferta: Oferta[]) => oferta);
+  }
 }
